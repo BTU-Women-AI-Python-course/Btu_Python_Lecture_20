3294c942433238ea6efd6d4ff6e79df6d9fa0e25
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-10-12 20:47:02 +0400

    add tests for books model creation

--- nikoloz_mdinaradze_lecture/books/tests.py
+++ nikoloz_mdinaradze_lecture/books/tests.py
@@ -1,11 +1,49 @@
 from django.urls import reverse
+from django.core.exceptions import ValidationError
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.test import TestCase
 
 from books.models import Book
 
 
-# Create your tests here.
+class BookModelTests(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="<NAME>",
+            publish_date="2024-10-12",
+        )
+
+    def test_book_creation(self):
+        self.assertEqual(self.book.title, "Test Book")
+        self.assertEqual(self.book.author, "<NAME>")
+        self.assertEqual(self.book.publish_date, "2024-10-12")
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.book), "Test Book")
+
+    def test_create_invalid_book_title(self):
+        invalid_title_data = {
+            "title": "A" * 101,  # max length is 100
+            "author": "<NAME>",
+            "publish_date": "2024-10-12",
+        }
+        book = Book(**invalid_title_data)
+        with self.assertRaises(ValidationError):
+            book.full_clean()  # This method validates model
+
+    def test_create_invalid_book_author(self):
+        invalid_author_data = {
+            "title": "Test Book",
+            "author": "A" * 101,
+            "publish_date": "2024-10-12",
+        }
+        book = Book(**invalid_author_data)
+        with self.assertRaises(ValidationError):
+            book.full_clean()  # This method validates model
+
+
 class BookTestCase(APITestCase):
     def setUp(self):
         self.book_payload = {
